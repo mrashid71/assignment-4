83ca9260501e91a748dce6c0c79dcf6603269fb8
mrashid71 <mamunoorrashid93@gmail.com>
2026-02-23 23:09:47 +0600

    counting updated

--- script.js
+++ script.js
@@ -21,3 +21,20 @@ function checkEmpty(count) {
     emptyMessage.classList.add("hidden");
   }
 }
+
+function updateCounts() {
+  cards = document.querySelectorAll("#allCards > div"); // refresh NodeList
+  let total = cards.length;
+  let interviewCount = 0;
+  let rejectedCount = 0;
+
+  cards.forEach(card => {
+    if (card.dataset.status === "interview") interviewCount++;
+    if (card.dataset.status === "rejected") rejectedCount++;
+  });
+
+  totalCountText.innerText = total;
+  jobsCountText.innerText = total + " jobs";
+  document.getElementById("interviewCount").innerText = interviewCount;
+  document.getElementById("rejectedCount").innerText = rejectedCount;
+}
